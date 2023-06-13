ff287c6cb78803139dbb0d896615d7a10658cb36
darshansatapara <109288106+darshansatapara@users.noreply.github.com>
2023-06-14 01:28:39 +0530

    starting front-end by toodo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Navbar from './component/Navbar';
+import About from './component/About';
+import Home from './component/Home';
 
 function App() {
   return (
     <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
