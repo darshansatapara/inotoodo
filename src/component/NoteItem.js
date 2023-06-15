ea1057b78d743bae852948902eddc6b2cae7750d
darshansatapara <109288106+darshansatapara@users.noreply.github.com>
2023-06-15 18:20:02 +0530

    alert massaage

--- src/component/NoteItem.js
+++ src/component/NoteItem.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const NoteItem=(props)=> {
+const NoteItem = (props) => {
     const { note } = props;
     return (
         <div className='col-md-3 my-2'>
             <div className="card" >
                 <div className="card-body" key={note.id}>
-                
+
+                    <div className="d-flex align-items-center">
                     <h5 className="card-title">{note.title}</h5>
-                    <p  className="card-text">{note.description}</p>
+                    <i className="fa-solid fa-trash-can mx-2"></i>
+                    <i className="fa-regular fa-pen-to-square mx-2"></i>
+                    </div>
+                    <p className="card-text">{note.description}</p>
                 </div>
             </div>
         </div>
